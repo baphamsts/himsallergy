0429b3400207deb1044a9e14385ebb7ec3ef27e9
Ba Pham <ba.pham@saigontechnology.com>
2019-03-07 18:49:01 +0700

    Backup Code

--- AllergyHistory/AllergyHistory.DAL/Repositories/AllegenHistoryRepository.cs
+++ AllergyHistory/AllergyHistory.DAL/Repositories/AllegenHistoryRepository.cs
@@ -1,4 +1,5 @@
 ﻿using AllergyHistory.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -64,34 +65,83 @@ namespace AllergyHistory.DAL.Repositories
 
             var data = new List<AllergenHistory>();
 
-            var domainDummyData = new AllergenHistory();
+            var fakeDomainEntity = new AllergenHistory();
+            fakeDomainEntity.Id = 1;
+            fakeDomainEntity.PatientId = 1;
+            fakeDomainEntity.PatientName = "Test Name ";
+            fakeDomainEntity.CISId = "0742031237";
+            fakeDomainEntity.DOB = DateTime.Now;
+            fakeDomainEntity.Gender = "Male";
+            fakeDomainEntity.Provider = "Redcedar";
+            fakeDomainEntity.Population = "Adult";
+            fakeDomainEntity.AxisI = "F25.0";
+            fakeDomainEntity.AxisIDesc = "Disoder";
+            fakeDomainEntity.ClientAllergyId = 1;
+            fakeDomainEntity.Allergen = "Reveal Blood";
+            fakeDomainEntity.deleted = false;
+            fakeDomainEntity.Notes = "Test notes";
+            fakeDomainEntity.CreateUser = "User 1";
+            fakeDomainEntity.CreateDate = DateTime.Now;
+            fakeDomainEntity.UpdateUser = "User 2";
+            fakeDomainEntity.UpdateDate = DateTime.Now;
+            fakeDomainEntity.Deleted = false;
+            fakeDomainEntity.AllergenId = 1;
+            fakeDomainEntity.AllergenType = 1;
+            fakeDomainEntity.ReactionId = 1;
+            fakeDomainEntity.SeverityId = 1;
+            fakeDomainEntity.ReactionDesc = "Diarrhea";
+            fakeDomainEntity.SeverityDesc = "Life Threatening";
+            fakeDomainEntity.Type = "Medication";
+            fakeDomainEntity.DrugName = "Vitro Strip";
+            fakeDomainEntity.CreateDateWithTime = DateTime.Now;
+            fakeDomainEntity.UpdateDateWithTime = DateTime.Now;
 
-            //domainDummyData.
-            //originFakeData.ClientName = "Patient Name";
-            //domainDummyData.Type = "Medication";
-            //domainDummyData.Allergen = "Amoxicillin";
-            //domainDummyData.Reaction = "Anaphylaxis";
-            //domainDummyData.Serverty = "Life Threatening";
-            //domainDummyData.Notes = "Test 123";
-            //domainDummyData.CreateInfo = "Created on 12/17/2018 by Stner";
-            //domainDummyData.UpdateInfo = "updated on 2/18/2019 by Rgarcia";
+
+            /*
+ * <row ClientId="29355" 
+ * ClientName="DOBIESZ,ALITA" 
+ * CISId="0742031237" 
+ * DOB="08/03/2001" 
+ * Gender="Male" 
+ * Provider="Redcedar" 
+ * Population="Children" 
+ * AxisI="F25.0" 
+ * AxisIDesc="Schizoaffective disorder,bipolar type" 
+ * ClientAllergyId="13399" 
+ * Allergen="Reveal Blood Glucose Test In Vitro Strip" 
+ * deleted="0" 
+ * Notes="Just don't let him use these, OK??? (edit 2)" 
+ * CreateUser="squinn" 
+ * CreateDate="02/08/2018" 
+ * UpdateUser="rkey" 
+ * UpdateDate="02/22/2018" 
+ * Deleted="0" 
+ * AllergenId="31993" 
+ * AllergenType="612" 
+ * ReactionId="616" 
+ * SeverityId="633" 
+ * ReactionDesc="Diarrhea" 
+ * SeverityDesc="Life Threatening" 
+ * Type="Medication" 
+ * DrugName="Reveal Blood Glucose Test In Vitro Strip" 
+ * CreateDateWithTime="2018-02-08T13:06:45.763" 
+ * UpdateDateWithTime="2018-02-22T09:09:14.690" />
+ */
+
 
             for (int i = 0; i < 10; i++)
             {
-                //// Dummy some differ data to observe
-                //var temp = new AllergenHistoryViewModel();
-                //temp.Patient = domainDummyData.Patient + (i + 1).ToString();
-                //temp.Type = i % 2 == 0 ? "Medication" : "Allegen";
-                //temp.Allergen = "Amoxicillin";
-                //temp.Reaction = "Anaphylaxis";
-                //temp.Serverty = "Life Threatening";
-                //temp.Notes = "Test";
-                //temp.CreateInfo = "Created on 12/17/2018 by X";
-                //temp.UpdateInfo = "updated on 2/18/2019 by Y";
-                //for (int j = 0; j < 10; j++)
-                //{
-                //    data.Add(temp);
-                //}
+                // Dummy some differ data to observe
+                var temp = new AllergenHistory();
+               
+                temp.Type = i % 2 == 0 ? "Medication" : "Allegen";
+                temp.Allergen = "Amoxicillin";
+
+
+                for (int j = 0; j < 10; j++)
+                {
+                    data.Add(temp);
+                }
             }
 
             return data;
--- AllergyHistory/AllergyHistory.Domain/Entities/AllergenHistory.cs
+++ AllergyHistory/AllergyHistory.Domain/Entities/AllergenHistory.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AllergyHistory.Domain.Entities
@@ -8,63 +9,33 @@ namespace AllergyHistory.Domain.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string PatientId { get; set; }
+        public int PatientId { get; set; }
         public string PatientName { get; set; }
         public string CISId { get; set; }
-        public string DOB { get; set; }
+        public DateTime DOB { get; set; }
         public string Gender { get; set; }
         public string Provider { get; set; }
         public string Population { get; set; }
         public string AxisI { get; set; }
-        public string ClientAllergyId { get; set; }
+        public string AxisIDesc { get; set; }
+        public int ClientAllergyId { get; set; }
         public string Allergen { get; set; }
-        public string deleted { get; set; }
+        public bool deleted { get; set; }
         public string Notes { get; set; }
         public string CreateUser { get; set; }
-        public string CreateDate { get; set; }
+        public DateTime CreateDate { get; set; }
         public string UpdateUser { get; set; }
-        public string UpdateDate { get; set; }
-        public string Deleted { get; set; }
-        public string AllergenId { get; set; }
-        public string AllergenType { get; set; }
-        public string ReactionId { get; set; }
-        public string SeverityId { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public bool Deleted { get; set; }
+        public int AllergenId { get; set; }
+        public int AllergenType { get; set; }
+        public int ReactionId { get; set; }
+        public int SeverityId { get; set; }
         public string ReactionDesc { get; set; }
         public string SeverityDesc { get; set; }
         public string Type { get; set; }
         public string DrugName { get; set; }
-        public string CreateDateWithTime { get; set; }
-        public string UpdateDateWithTime { get; set; }
+        public DateTime CreateDateWithTime { get; set; }
+        public DateTime UpdateDateWithTime { get; set; }
     }
 }
-
-/*
- * <row ClientId="29355" 
- * ClientName="DOBIESZ,ALITA" 
- * CISId="0742031237" 
- * DOB="08/03/2001" 
- * Gender="Male" 
- * Provider="Redcedar" 
- * Population="Children" 
- * AxisI="F25.0" 
- * AxisIDesc="Schizoaffective disorder,bipolar type" 
- * ClientAllergyId="13399" 
- * Allergen="Reveal Blood Glucose Test In Vitro Strip" 
- * deleted="0" 
- * Notes="Just don't let him use these, OK??? (edit 2)" 
- * CreateUser="squinn" 
- * CreateDate="02/08/2018" 
- * UpdateUser="rkey" 
- * UpdateDate="02/22/2018" 
- * Deleted="0" 
- * AllergenId="31993" 
- * AllergenType="612" 
- * ReactionId="616" 
- * SeverityId="633" 
- * ReactionDesc="Diarrhea" 
- * SeverityDesc="Life Threatening" 
- * Type="Medication" 
- * DrugName="Reveal Blood Glucose Test In Vitro Strip" 
- * CreateDateWithTime="2018-02-08T13:06:45.763" 
- * UpdateDateWithTime="2018-02-22T09:09:14.690" />
- */
